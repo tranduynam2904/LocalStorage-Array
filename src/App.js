5eee9ddda5c960e1094b4479f93b4eda55398833
tranduynam2904 <tranduynam2904@gmail.com>
2023-04-22 18:50:39 +0700

    update lan 1 do meo up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState, useEffect } from "react";
 function App() {
+  const [Name, setName] = useState("");
+  const [old, setOld] = useState("");
+  const example = [{ name: "cat", old: 5 }]
+  const handle_change_name = (e) => {
+    setName(e.target.value)
+  }
+  const handle_change_old = (e) => {
+    setOld(e.target.value)
+  }
+  const [animal, setAnimal] = useState(() => {
+    let animalLocal;
+    if (localStorage.getItem("animal")) {
+      animalLocal = JSON.parse(localStorage.getItem("animal"))
+    }
+    else{
+      animalLocal = example;
+    }
+    return animalLocal;
+  })
+  const handle_add_animal = () => {
+    setAnimal((pre) => {
+      const newanimal = [...pre, { name: Name, old: old }]
+      localStorage.setItem("animal", JSON.stringify(newanimal));
+      return newanimal;
+    })
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type='text' placeholder='Name' value={Name} onChange={handle_change_name} />
+      <input type='text' placeholder='Old' value={old} onChange={handle_change_old} />
+      <ul>
+        {animal.map((st, key) => {
+          return <li>{st.name + " " + st.old}</li>
+        })}
+      </ul>
+      <button onClick={handle_add_animal}>Add Animal</button>
     </div>
-  );
+  )
 }
 
 export default App;
